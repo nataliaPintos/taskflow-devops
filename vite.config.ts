da9a92a9d86e820bf259a416f05294c5057b0aa3
Nat <natalia@MacBook-Air-de-Nat.local>
2026-08-21 23:57:16 -0300

    fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'node:url'
-import { dirname, resolve } from 'node:path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/test/setup.ts',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
   },
 })
